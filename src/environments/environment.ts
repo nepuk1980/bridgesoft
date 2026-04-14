ead89e3706b9fd8a8d1c0213b5c54cb154edfd51
nepuk1980 <137596064+nepuk1980@users.noreply.github.com>
2026-04-14 14:21:03 +0530

    API Base URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://34.196.122.210:8080/fasm',
+  apiUrl: 'https://fasm.bridgesoft.com/fasm',
   //   token:
   //     'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTc3NDQyODMxNSwiZXhwIjoxNzc0NDM1NTE1fQ.NNnDvlWtyGig0kQ67WPyJ1T5zpSTQQbyx7yXdppFhqs',
   username: 'admin',
